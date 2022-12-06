523e27cbf6f8bf1bf37b5a26b5317e6890f24fa2
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2022-12-07 01:38:04 +0530

    fix: add cors

--- app/api.py
+++ app/api.py
@@ -4,8 +4,25 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 from app.models.user import User
 from app.routes.user import user_router
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://0.0.0.0:8080",
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router, tags=["user"], prefix="/user")
 
