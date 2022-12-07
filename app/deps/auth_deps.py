c6cf3c3905484d4e2b36b51d26ada86e847d90bf
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2022-12-07 11:21:39 +0530

    fix - update actions

--- app/deps/auth_deps.py
+++ app/deps/auth_deps.py
@@ -9,11 +9,13 @@ from pydantic import BaseModel, ValidationError
 from app.models.user import User
 from app.utils import ALGORITHM, SECRET_KEY
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/auth/login', scheme_name="JWT")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login", scheme_name="JWT")
+
 
 class TokenPayload(BaseModel):
     sub: UUID
     exp: int
+
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     # decode the payload and get the user id
@@ -25,18 +27,19 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         user_id = TokenPayload(**payload).sub
 
         # return payload
-    except(jwt.JWTError, ValidationError):
+    except (jwt.JWTError, ValidationError):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
-            detail="not able to validate credentials"
+            detail="not able to validate credentials",
         )
-    
+
     user = await User.find_one(User.user_id == user_id)
 
     if not user:
-        raise(HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="user not found"
-        ))
+        raise (
+            HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="user not found"
+            )
+        )
     return user
     # pass
--- app/models/user.py
+++ app/models/user.py
@@ -10,14 +10,17 @@ class User(Document):
     Args:
         Document (_type_): _description_
     """
+
     user_id: UUID = Field(default_factory=uuid4)
-    email: Indexed(EmailStr, unique=True) # EmailStr -> need to understand more about this 
-    username:Indexed(str, unique=True)
-    hashed_password:str
-    is_active:bool = True
+    email: Indexed(
+        EmailStr, unique=True
+    )  # EmailStr -> need to understand more about this
+    username: Indexed(str, unique=True)
+    hashed_password: str
+    is_active: bool = True
 
     # 👇 collection is used at the time of creating the user document in the db
-    class Collection: 
-        """_summary_
-        """
+    class Collection:
+        """_summary_"""
+
         name = "users"
